c4cacdb1ed2e94959a54eece6502945b45ddb5b8
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-05-18 21:01:12 +0330

    adding functionality to the page

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const userInput = document.querySelector("input");
+const container = document.querySelector(".container");
 
 async function getUser(name) {
   try {
     const response = await axios.get(`https://api.github.com/users/${name}`);
-    return response.data;
+    return response;
   } catch (error) {
     console.error(error);
   }
@@ -19,13 +20,38 @@ async function getrepos(name) {
     console.error(error);
   }
 }
-
+let myname = "loyeh";
+// getUser(myname);
 userInput.addEventListener("keypress", (e) => {
   if (e.key == "Enter") {
-    console.log(e.key);
+    // console.log(e.key);
     const userName = userInput.value;
     const userInfo = getUser(userName);
     const userRepos = getrepos(userName);
     console.log(userInfo, userRepos);
+    if (userInfo.data) {
+      container.innerHTML = ` <div class="image"></div>
+    <div class="user">
+      <h3>${userInfo.data.name}</h3>
+      <div>
+        ${userInfo.data.bio}
+      </div>
+      <div class="info">
+        <div>${userInfo.data.followers} Followers</div>
+        <div>${userInfo.data.following} Following</div>
+        <div>${userInfo.data.public_repos} Repos</div>
+      </div>
+      <div class="repos">
+        <div>${userRepos.data[0].name}</div>
+        <div>${userRepos.data[1].name}</div>
+        <div>${userRepos.data[2].name}</div>
+        <div>${userRepos.data[3].name}</div>
+        <div>${userRepos.data[4].name}</div>
+        <div>${userRepos.data[5].name}</div>
+      </div>
+    </div>`;
+    } else {
+      container.innerHTML = `<h3>There is no User by This Name</h3>`;
+    }
   }
 });
