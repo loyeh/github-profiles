4aa0e9f0cf8012123c2d607147a7b6b4d89bea71
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-05-18 19:06:02 +0330

    add await functions

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-// const axios = require("axios");
-// import axios from "axios";
-// const axios = require("axios/dist/browser/axios.cjs");
+const userInput = document.querySelector("input");
 
-axios
-  .get("https://api.github.com/users/loyeh ")
-  .then((resp) => console.log(resp.data))
-  .catch((err) => console.log(err));
+async function getUser(name) {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${name}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-axios
-  .get("https://api.github.com/users/loyeh/repos ")
-  .then((resp) => console.log(resp.data))
-  .catch((err) => console.log(err));
+async function getrepos(name) {
+  try {
+    const response = await axios.get(
+      `https://api.github.com/users/${name}/repos`
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+userInput.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    console.log(e.key);
+    const userName = userInput.value;
+    const userInfo = getUser(userName);
+    const userRepos = getrepos(userName);
+    console.log(userInfo, userRepos);
+  }
+});
